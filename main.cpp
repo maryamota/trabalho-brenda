bc9a6dc725c16b36d7936ac1ebbc68573c4f41eb
Maryamota <marya.e.mota@gmail.com>
2026-06-01 16:30:20 -0300

    obejetivos1.2

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main() {
 
 
  
- cout << "Seja muito bem vindo(a), aventureiro(a)! Escolha um objetivo para sua jornada: " << endl;
- cout << "1 . Luz e calor: voce deve coletar 4 gravetos e 2 pedras." << endl;
- cout << "2 . Comida: voce deve coletar 1 fruta e 3 animais." << endl;
- cout << "3 . Abrigo: voce deve coletar 5 gravetos e 4 pedras." << endl;
- cout << "4 . Construção: voce deve coletar 8 pedras e 4 árvores;" << endl;
+ cout << "Seja muito bem vindo(a), aventureiro(a)! Escolha um objetivo para sua jornada:\n";
+ cout << "   1. Luz e calor: voce deve coletar 4 gravetos e 2 pedras.\n";
+ cout << "   2. Comida: voce deve coletar 1 fruta e 3 animais.\n";
+ cout << "   3. Abrigo: voce deve coletar 5 gravetos e 4 pedras.\n";
+ cout << "   4. Construção: voce deve coletar 8 pedras e 4 árvores.\n";
  cin >> obejtivoescolhido;
  
 }
