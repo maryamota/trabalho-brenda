980b5b0feb29f806520fb88b243f7b6da1960180
Maryamota <marya.e.mota@gmail.com>
2026-06-01 16:16:17 -0300

    objetivos

--- main.cpp
+++ main.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+int main() {
+ int  obejtivoescolhido = 0;
+
+
+
+ 
+ cout << "Seja muito bem vindo(a), aventureiro(a)! Escolha um objetivo para sua jornada: " << endl;
+ cout << "1 . Luz e calor: voce deve coletar 4 gravetos e 2 pedras." << endl;
+ cout << "2 . Comida: voce deve coletar 1 fruta e 3 animais." << endl;
+ cout << "3 . Abrigo: voce deve coletar 5 gravetos e 4 pedras." << endl;
+ cout << "4 . Construção: voce deve coletar 8 pedras e 4 árvores;" << endl;
+ cin >> obejtivoescolhido;
+ 
+}
